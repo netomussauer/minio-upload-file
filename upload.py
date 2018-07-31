af5c6170194a27465da1a3656b8c11241fb3c866
Jose Mussauer | Stone <jose.mussauer@stone.com.br>
2018-07-31 11:38:22 -0300

    add python script to create a new bucket and upload some file into minio S3 storage

---
+++ upload.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from minio import Minio
+from minio.error import *
+import getpass
+import os
+
+
+def minio_client(minio_url, minio_access, minio_secret):
+  minioClient = Minio(minio_url, access_key=minio_access, secret_key=minio_secret, secure=True)
+  return minioClient
+
+
+def create_bucket(minio_client, bucket_name):
+  try:
+    minio_client.make_bucket(bucket_name, location='us-east-1')
+  except BucketAlreadyOwnedByYou as err:
+    print(err.message)
+
+
+def file_upload(minio_client, bucket_name, file_path):
+  try:
+    with open(file_path,'rb') as file_data:
+      file_stat = os.stat(file_path)
+      print(minio_client.put_object(bucket_name, os.path.basename(file_path), file_data, file_stat.st_size))
+      print('File upload sucessfully!')
+  except ResponseError as err:
+    print(err)
+
+
+def main():
+  url = input('Enter Minio URL: ') 
+  access = input('Enter Minio Access Key: ')
+  key = getpass.getpass('Enter Minio Password: ')
+  bucket = input('Enter Bucket Name: ')
+  miniocli = minio_client(url, access, key)
+  create_bucket(miniocli, bucket)
+  file_path = input('Enter File Path: ')
+  file_upload(miniocli, bucket, file_path)
+
+
+if __name__ == '__main__':
+  main()
